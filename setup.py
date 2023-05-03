f4e3a569588be38d989c21daab2e05e5e994b510
3ll3d00d <mattkhan@gmail.com>
2023-05-03 22:49:46 +0100

    bump deps

--- setup.py
+++ setup.py
@@ -33,45 +33,46 @@ setup(
         ],
     },
     install_requires=[
-        "aniso8601==9.0.1; python_version >= '3.5'",
-        "attrs==22.1.0; python_version >= '3.5'",
-        "autobahn[twisted]==22.7.1",
-        "automat==20.2.0",
-        "certifi==2022.6.15.1; python_full_version >= '3.6.0'",
-        "cffi==1.15.1",
-        "charset-normalizer==2.1.1; python_full_version >= '3.6.0'",
-        "click==8.1.3; python_version >= '3.7'",
-        "constantly==15.1.0",
-        "cryptography==38.0.1; python_full_version >= '3.6.0'",
-        "flask==2.1.3",
-        "flask-restx==0.5.1",
-        "hyperlink==21.0.0",
-        "idna==3.3; python_version >= '3.5'",
-        "importlib-metadata==4.12.0; python_version < '3.10'",
-        "incremental==21.3.0",
-        "itsdangerous==2.1.2; python_version >= '3.7'",
-        "jinja2==3.1.2; python_version >= '3.7'",
-        "jsonschema==4.16.0; python_version >= '3.7'",
-        "markupsafe==2.1.1; python_version >= '3.7'",
-        "netifaces==0.11.0",
-        "pillow==9.2.0; python_version >= '3.7'",
-        "plumbum==1.7.2",
-        "pycparser==2.21; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
-        "pymcws==1.1.0",
-        "pyrsistent==0.18.1; python_version >= '3.7'",
-        "pytz==2022.2.1",
-        "pyyaml==6.0",
-        "requests==2.28.1",
-        "setuptools==65.3.0; python_version >= '3.7'",
-        "six==1.16.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
-        "twisted==22.8.0; python_full_version >= '3.7.1'",
-        "txaio==22.2.1; python_full_version >= '3.6.0'",
-        "typing-extensions==4.3.0; python_version >= '3.7'",
-        "urllib3==1.26.12; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4, 3.5' and python_version < '4'",
-        "wakeonlan==2.1.0",
-        "werkzeug==2.1.2",
-        "zipp==3.8.1; python_version >= '3.7'",
-        "zope.interface==5.4.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+        "aniso8601==9.0.1; python_version >= '3.5'"
+        "attrs==23.1.0; python_version >= '3.5'"
+        "autobahn[twisted]==23.1.2"
+        "automat==22.10.0"
+        "blinker==1.6.2; python_full_version >= '3.7.0'"
+        "certifi==2022.12.7; python_version >= '3.6'"
+        "cffi==1.15.1"
+        "charset-normalizer==3.1.0; python_full_version >= '3.7.0'"
+        "click==8.1.3; python_full_version >= '3.7.0'"
+        "constantly==15.1.0"
+        "cryptography==40.0.2; python_version >= '3.6'"
+        "flask==2.3.2"
+        "flask-restx==1.1.0"
+        "hyperlink==21.0.0"
+        "idna==3.4; python_version >= '3.5'"
+        "importlib-metadata==6.6.0; python_version < '3.10'"
+        "incremental==22.10.0"
+        "itsdangerous==2.1.2; python_full_version >= '3.7.0'"
+        "jinja2==3.1.2; python_full_version >= '3.7.0'"
+        "jsonschema==4.17.3; python_full_version >= '3.7.0'"
+        "markupsafe==2.1.2; python_full_version >= '3.7.0'"
+        "netifaces==0.11.0"
+        "pillow==9.5.0; python_full_version >= '3.7.0'"
+        "plumbum==1.8.1"
+        "pycparser==2.21; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'"
+        "pymcws==1.1.0"
+        "pyrsistent==0.19.3; python_full_version >= '3.7.0'"
+        "pytz==2023.3"
+        "pyyaml==6.0"
+        "requests==2.29.0"
+        "setuptools==67.7.2; python_full_version >= '3.7.0'"
+        "six==1.16.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'"
+        "twisted==22.10.0; python_full_version >= '3.7.1'"
+        "txaio==23.1.1; python_full_version >= '3.7.0'"
+        "typing-extensions==4.5.0; python_full_version >= '3.7.0'"
+        "urllib3==1.26.15; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4, 3.5'"
+        "wakeonlan==3.0.0"
+        "werkzeug==2.3.3"
+        "zipp==3.15.0; python_full_version >= '3.7.0'"
+        "zope.interface==6.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'"
     ],
     tests_require=["pytest", "pytest-httpserver"],
     include_package_data=True,
