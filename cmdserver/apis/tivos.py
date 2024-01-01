d4e232369f220dac885cec1eebc5678178052797
3ll3d00d <mattkhan@gmail.com>
2024-01-01 18:19:29 +0000

    add debug logging to pick up tivo response time

--- cmdserver/apis/tivos.py
+++ cmdserver/apis/tivos.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from flask import request
 from flask_restx import Resource, Namespace
@@ -23,11 +24,14 @@ class Tivos(Resource):
         type = payload['type']
         name = payload['name']
         command = payload['command']
+        start = time.time()
         logger.debug('Sending ' + type + '/' + command + ' to ' + name)
         if self.__controller.has_tivo(name):
             try:
                 sent = self.__controller.send(name, type, command)
+                end = time.time()
                 # sent: [] of commands, channel: current channel text, type, name, command
+                logger.debug(f'Responding with {sent} in {round((end-start) / 1000, 3)} ms')
                 return {**sent, **payload}, 200
             except Exception as e:
                 return {'error': str(e), **payload}, 500
